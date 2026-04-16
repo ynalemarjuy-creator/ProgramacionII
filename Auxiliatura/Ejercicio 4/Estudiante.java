c93fbe27f56610ae5d3eaf584afa25c700461fbe
ynalemarjuy <ynalemarjuy@gmail.com>
2026-04-15 21:23:18 -0400

    meti una carpeta ejercicio 4

---
+++ Auxiliatura/Ejercicio 4/Estudiante.java
@@ -0,0 +1,26 @@
+public class Estudiante extends Persona {
+    private int matricula;
+    private String carrera;
+
+    public Estudiante(String nombre, int carnet, int edad, int matricula, String carrera) {
+        super(nombre, carnet, edad);
+        this.matricula = matricula;
+        this.carrera = carrera;
+    }
+
+    @Override
+    public void mostrar() {
+        super.mostrar();
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Carrera: " + carrera);
+        System.out.println("----------------------");
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+}
