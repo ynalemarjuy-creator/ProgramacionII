d69c5e4d3da7d66ed7a83b78ad34c25b6ed0b4d2
ynalemarjuy <ynalemarjuy@gmail.com>
2026-04-15 22:02:14 -0400

    actualizazcion

--- Auxiliatura/Ejercicio4/Main2.java
+++ Auxiliatura/Ejercicio4/Main2.java
@@ -1,10 +1,10 @@
-public class Aplicacion {
+public class Main2 {
 
     public static Figura mayorArea(Figura f1, Figura f2) {
         return (f1.obtenerArea() > f2.obtenerArea()) ? f1 : f2;
     }
 
-    public static void aplicacion(String[] args) {
+    public static void main(String[] args) {
 
         Cuadrado c = new Cuadrado("Rojo", 4);
         Triangulo t = new Triangulo("Verde", 3, 4, 5);
